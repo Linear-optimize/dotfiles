d2a0083c25c11a93370ab67c1166bafac358027f
rene <linear23150@outlook.com>
2026-03-22 16:38:05 +0800

    fix:change dockerfile of rust

--- Docker_Config/Rust/build.sh
+++ Docker_Config/Rust/build.sh
@@ -1,40 +1,25 @@
 #!/bin/sh
-
 set -x
-
-mkdir -vp ${CARGO_HOME:-$HOME/.cargo}
-
-cat << EOF | tee -a ${CARGO_HOME:-$HOME/.cargo}/config.toml
-[source.crates-io]
-replace-with = 'ustc'
-
-[source.ustc]
-registry = "sparse+https://mirrors.ustc.edu.cn/crates.io-index/"
-
-[registries.ustc]
-index = "sparse+https://mirrors.ustc.edu.cn/crates.io-index/"
-EOF
-
 
 sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
 
-apt-get update
-apt-get install -y \
-    gcc-x86-64-linux-gnu \
-    fish \
-    curl \
-    git
+apt-get update && apt-get install -y \
+    gcc-x86-64-linux-gnu fish curl git pkg-config libssl-dev
 
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+mkdir -p /etc/fish && echo "starship init fish | source" >> /etc/fish/config.fish
 
-mkdir -p $HOME/.config/fish
-echo "starship init fish | source" >> $HOME/.config/fish/config.fish
+
+mkdir -p $CARGO_HOME
+cat << EOF > $CARGO_HOME/config.toml
+[source.crates-io]
+replace-with = 'ustc'
+[source.ustc]
+registry = "sparse+https://mirrors.ustc.edu.cn/crates.io-index/"
+EOF
 
 rustup target add x86_64-unknown-linux-musl
 
-cargo install cargo-watch
 
-apt-get autoclean
-rm -rf /var/lib/apt/lists/*
-rm -rf /tmp/*
+apt-get autoclean && rm -rf /var/lib/apt/lists/*
